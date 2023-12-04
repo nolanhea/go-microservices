8e24ff759df08f15f416b40abede949a66ca2296
ramir512 <ramir.khvan@gmail.com>
2023-12-04 22:03:06 +0300

    Add logger-service

---
+++ logger-service/data/models.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
